13ca12f03d816f7e619e34083883998d66fa8160
dlaz <dlaz@gmail.com>
2025-08-23 11:46:35 +0330

    third update

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,11 +3,21 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import AppContextProvider from './context/AppContext.jsx'
 import './index.css'
+import { ClerkProvider } from '@clerk/clerk-react'
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Add your Clerk Publishable Key to the .env file')
+}
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <AppContextProvider>
-      <App />
-    </AppContextProvider>
-  </BrowserRouter>,
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+    <BrowserRouter>
+      <AppContextProvider>
+        <App />
+      </AppContextProvider>
+    </BrowserRouter>
+  </ClerkProvider>
+  ,
 )
